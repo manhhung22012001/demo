c6c1890337043903bc36384c0fcaf7cdab58a77d
manhhung22012001 <tranmanhhung2201@gmail.com>
2023-10-13 23:07:38 +0700

    ai fix?

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { HttpErrorResponse } from '@angular/common/http'; // Import HttpErrorResponse
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -23,10 +24,19 @@ export class RegisterComponent implements OnInit {
   register() {
     this.authService.register(this.registerForm.value.username, this.registerForm.value.password, this.registerForm.value.fullname, this.registerForm.value.phonenumber, this.registerForm.value.role).subscribe(response => {
       var code = response.status;
-      if (code == 201)
+      if (code === 201) {
         this.message = "Đăng ký thành công";
-      else {
+      } else {
         this.message = "Đăng ký thất bại";
+      }
+    },
+    (error: HttpErrorResponse) => { // Handle errors
+      if (error.status === 400) {
+        this.message = "Dữ liệu không hợp lệ";
+      } else if (error.status === 404) {
+        this.message = "Tài khoản đã tồn tại";
+      } else {
+        this.message = "Đã xảy ra lỗi. Vui lòng thử lại sau.";
       }
     }
     );
